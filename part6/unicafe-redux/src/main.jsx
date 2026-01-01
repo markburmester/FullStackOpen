ad970ceacd29434bf47df022fc88f61e61303dbc
Mark Burmester Azcarraga <mark.burmester.2002@gmail.com>
2026-01-01 13:39:52 +0100

    Agregar ejercicio unicafe-redux con Redux Toolkit

---
+++ part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,30 @@
+import ReactDOM from 'react-dom/client'
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './reducers/counterReducer'
+
+const store = configureStore({
+  reducer: counterReducer
+})
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'RESET' })}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
